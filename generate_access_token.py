a8417dea9f30fa714c13a49fdbb7f49da96cd4c0
Marcelo Cueto <cueto@live.cl>
2020-01-06 18:50:48 -0300

    Add generate_access_token script

---
+++ generate_access_token.py
@@ -0,0 +1,33 @@
+import os
+import json
+import http.client
+from dotenv import (
+    load_dotenv,
+)
+
+load_dotenv()
+
+conn = http.client.HTTPSConnection(os.environ.get('AUTH0_DOMAIN'))
+
+payload = {
+    'client_id': os.environ.get('AUTH0_CLIENT_ID'),
+    'client_secret': os.environ.get('AUTH0_CLIENT_SECRET'),
+    'audience': os.environ.get('AUTH0_AUDIENCE'),
+    'grant_type': 'client_credentials'
+}
+
+headers = {
+    'content-type': "application/json"
+}
+
+conn.request(
+    "POST",
+    "/oauth/token",
+    json.dumps(payload),
+    headers
+)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
